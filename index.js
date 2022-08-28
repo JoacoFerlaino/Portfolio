9ff3e455405462c5defed44ce8b458097385e458
Joaquin <drjoaquinferlaino@gmail.com>
2022-08-28 19:27:32 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,45 @@
+//About section
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+//Hamburguer menu
+const sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+//Form Google Sheet
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzVSFbwyDxf_Lf-JU7Fa7E_Ci1IobrRisQSmMM6dcOg29ZLf97PDmM4pXs_jPUT4Nu4Dw/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")  
+
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent succesfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+  /////////
